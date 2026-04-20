17a4ad39c0bffcf76023f6f739a7a5618da7e201
Raizel <abcd61703@gmail.com>
2026-04-20 17:43:50 +0900

    Initial Commit

---
+++ promptpop/promptpopApp.swift
@@ -0,0 +1,17 @@
+//
+//  promptpopApp.swift
+//  promptpop
+//
+//  Created by Raizel H. on 2026/4/20.
+//
+
+import SwiftUI
+
+@main
+struct promptpopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
